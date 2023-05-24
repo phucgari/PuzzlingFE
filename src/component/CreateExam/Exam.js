ee28ddfc9afa91aca4446cac0c3ab0b94731e7aa
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-05-24 09:19:07 +0700

    update

--- src/component/CreateExam/Exam.js
+++ src/component/CreateExam/Exam.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export default function Exam() {
     const [exam, setExam] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
             axios.get(`http://localhost:8080/puzzling/exam/list?username=${JSON.parse(localStorage.getItem("account")).username}`)
@@ -20,18 +21,20 @@ export default function Exam() {
             <div className="row">
                 {
                     exam.map((item) => (
+                        <a onClick={() => navigate("/exam/edit", {state:{id:item.id}})}>
                             <div
                                 className="col-auto col-centered animated wow jackInTheBox slow"
                                 data-toggle="modal"
                                 data-target="#subCateModal"
                                 key={item.id}
                             >
-                                    <div className="box-part text-center shadow">
-                                        <div className="title mt-4">
-                                            <h4>{item.name}</h4>
-                                        </div>
+                                <div className="box-part text-center shadow">
+                                    <div className="title mt-4">
+                                        <h4>{item.name}</h4>
                                     </div>
+                                </div>
                             </div>
+                        </a>
                     ))
                 }
                 <div
