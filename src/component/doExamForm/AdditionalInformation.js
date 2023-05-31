b1b9541262750cac0108a3180f661c98588aa32a
HuyNQ0609 <nguyenhuy06092001@gmail.com>
2023-05-31 08:08:22 +0700

    update form DoExam

--- src/component/doExamForm/AdditionalInformation.js
+++ src/component/doExamForm/AdditionalInformation.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import AuthorAndLevel from "./AdditionalInformation/AuthorAndLevel";
 import QuestionPaging from "./AdditionalInformation/QuestionPaging";
-import Clock from "./AdditionalInformation/Clock";
 
 function AdditionalInformation(props) {
-    const{formik,currentIndex,setCurrentIndex}=props
+    const {formik, currentIndex, setCurrentIndex} = props
     return (
-        <div>
-            <AuthorAndLevel
-                formik={formik}
-                currentIndex={currentIndex}
-            />
+        <div style={{display:"flex", justifyContent:"center"}}>
             <QuestionPaging
                 formik={formik}
                 currentIndex={currentIndex}
                 setCurrentIndex={setCurrentIndex}
-            />
-            <Clock
-                formik={formik}
             />
         </div>
     );
--- src/component/doExamForm/DoExamForm.js
+++ src/component/doExamForm/DoExamForm.js
@@ -4,11 +4,14 @@ import axios from "axios";
 import Render1Question from "./Render1Question";
 import {Form, Formik} from "formik";
 import AdditionalInformation from "./AdditionalInformation";
+import Clock from "./AdditionalInformation/Clock";
+import AuthorAndLevel from "./AdditionalInformation/AuthorAndLevel";
 
 function DoExamForm(props) {
-    const { examId } = useParams();
+    const {examId} = useParams();
     const navigate = useNavigate();
-    function submitRecord(values){
+
+    function submitRecord(values) {
         axios.post(`http://localhost:8080/puzzling/record/createExamResult`, values)
             .then(() => {
                 navigate("/")
@@ -17,10 +20,11 @@ function DoExamForm(props) {
                 console.log(error);
             });
     }
-    const[record,setRecord]= React.useState(
+
+    const [record, setRecord] = React.useState(
         {
-            user:{
-                id:JSON.parse(localStorage.getItem("id"))
+            user: {
+                id: JSON.parse(localStorage.getItem("id"))
             },
             exam: {
                 id: "",
@@ -29,7 +33,7 @@ function DoExamForm(props) {
                 passScore: 0,
                 questions: [{
                     name: "",
-                    level:"",
+                    level: "",
                     options: [{
                         name: ""
                     }]
@@ -37,81 +41,109 @@ function DoExamForm(props) {
                 category: "",
                 user: {}
             },
-            recordDetail:[
+            recordDetail: [
                 {
-                    question:{},
-                    answers:[
+                    question: {},
+                    answers: [
                         {
-                            option:{},
-                            answerStatus:"false"
+                            option: {},
+                            answerStatus: "false"
                         }
                     ]
                 }
             ]
         });
-    const[currentIndex,setCurrentIndex]=React.useState(0);
-    useEffect(()=>{
+    const [currentIndex, setCurrentIndex] = React.useState(0);
+    useEffect(() => {
         axios.get(`http://localhost:8080/puzzling/exam/info?examId=${examId}`)
             .then((response) => {
-                setRecord(  {
-                        user:{
-                            id:JSON.parse(localStorage.getItem("id"))
-                        },
-                        exam: response.data,
-                        recordDetail: response.data.questions.map((question)=>({
-                            question: question,
-                            answers:question.options.map((option)=>({
-                                option:option,
-                                answerStatus:"false"
-                            }))
+                setRecord({
+                    user: {
+                        id: JSON.parse(localStorage.getItem("id"))
+                    },
+                    exam: response.data,
+                    recordDetail: response.data.questions.map((question) => ({
+                        question: question,
+                        answers: question.options.map((option) => ({
+                            option: option,
+                            answerStatus: "false"
                         }))
-                    })
+                    }))
+                })
             })
             .catch((error) => {
                 console.log(error);
             })
-    },[examId])
+    }, [examId])
     return (
-        <div>
-            <Formik initialValues={record}
-                    onSubmit={submitRecord}
-                    enableReinitialize={true}
-            >
-                {
-                    (formik)=>{
-                        return(
-                            <Form>
-                                <Render1Question
-                                    currentIndex={currentIndex}
-                                    formik={formik}
-                                />
-                                {
-                                    currentIndex!==record.exam.questions.length-1 &&
-                                    <button type="button" onClick={() => setCurrentIndex((cur) => ++cur)} className="btn btn-primary">
-                                        Câu hỏi tiếp theo
-                                    </button>
-                                }
-                                {
-                                    currentIndex===record.exam.questions.length-1 &&
-                                    <button type="submit" className="btn btn-primary">
-                                        Nộp bài thi
-                                    </button>
-                                }
-                                {currentIndex!==0 &&
-                                    <button type="button" onClick={()=>setCurrentIndex((cur)=>--cur)} className="btn btn-primary">
-                                        Câu hỏi trước
-                                    </button>
-                                }
-                                <AdditionalInformation
-                                    formik={formik}
-                                    currentIndex={currentIndex}
-                                    setCurrentIndex={setCurrentIndex}
-                                />
-                            </Form>
-                        )
-                    }
-                }
-            </Formik>
+        <div className="container mt-5">
+            <div className="modal-dialog modal-xl" role="document">
+                <div className="modal-content rounded-modal shadow p-4 border-0"
+                     style={{backgroundColor: "#bef6fd"}}>
+                    <Formik initialValues={record}
+                            onSubmit={submitRecord}
+                            enableReinitialize={true}
+                    >
+                        {
+                            (formik) => {
+                                return (
+                                    <Form>
+                                        <div className={"row"}>
+                                            <div className={"col col-6"}>
+                                                <Clock
+                                                    formik={formik}
+                                                />
+                                            </div>
+                                            <div className={"col col-6"} style={{textAlign: "right"}}>
+                                                <AuthorAndLevel
+                                                    formik={formik}
+                                                    currentIndex={currentIndex}
+                                                />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <Render1Question
+                                                currentIndex={currentIndex}
+                                                formik={formik}
+                                            />
+                                        </div>
+                                        <AdditionalInformation
+                                            formik={formik}
+                                            currentIndex={currentIndex}
+                                            setCurrentIndex={setCurrentIndex}
+                                        />
+                                        <div>
+                                            {currentIndex !== 0 &&
+                                                <button type="button"
+                                                        onClick={() => setCurrentIndex((cur) => --cur)}
+                                                        className="gradientBtn mt-4 animated wow fadeInUp">
+                                                    Câu hỏi trước
+                                                </button>
+                                            }
+                                            {
+                                                currentIndex !== record.exam.questions.length - 1 &&
+                                                <button type="button"
+                                                        onClick={() => setCurrentIndex((cur) => ++cur)}
+                                                        className="gradientBtn mt-4 animated wow fadeInUp">
+                                                    Câu hỏi tiếp theo
+                                                </button>
+                                            }
+                                            {
+                                                currentIndex === record.exam.questions.length - 1 &&
+                                                <button type="submit"
+                                                        className="gradientBtn mt-4 animated wow fadeInUp">
+                                                    Nộp bài thi
+                                                </button>
+                                            }
+                                        </div>
+                                    </Form>
+                                )
+                            }
+                        }
+                    </Formik>
+
+                </div>
+            </div>
         </div>
     );
 }
