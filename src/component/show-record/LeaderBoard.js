58db8cab09cb0164e79e3d23ac6255c6f6264a91
MacOS <macos@SowZ.local>
2023-06-05 08:59:25 +0700

    son pull

--- src/component/show-record/LeaderBoard.js
+++ src/component/show-record/LeaderBoard.js
@@ -1,52 +1,63 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
-function LeaderBoard(props) {
+function LeaderBoard() {
     const {examId} = useParams();
-    const {record} = props
-    const [index, setIndex] = useState(0);
     const [leaderBoard, setLeaderBoard] = useState([])
+    console.log(leaderBoard)
     useEffect(() => {
-        setIndex(prevIndex => prevIndex + 1);
-        axios.get("http://localhost:8080/puzzling/record/leaderboard/"+ examId)
+            axios.get("http://localhost:8080/puzzling/record/leaderboard/" + examId)
                 .then((response) => {
                     setLeaderBoard(response.data)
                 })
                 .catch((error) => {
                     console.log(error.message)
                 })
-        }, [examId]
+        }, []
     )
     return (
-        <div>
-            {
-                leaderBoard.map((item) =>(
-                    <div className="light-bg home-bg"  key={item.id}>
-                        <div className="container">
-                            <div className="col-auto col-centered leaderboard table-responsive">
-                                <table className="table">
-                                    <tbody>
-                                    <tr className="shadow bg-white animated wow fadeInDown delay-0-3s">
-                                        <th scope="row" className="color-blue">{index}</th>
-                                        <td><img src={item.picture}
-                                                 className="rounded-circle bg-green ranker-profile"/></td>
-                                        <td>
-                                            <h6 className="text-dark">{item.username}</h6>
-                                        </td>
-                                        <td>
-                                            <h5 className="color-purple"><b>{item.record.userPoint}</b></h5>
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+        <div className={"container"}>
+            <div className="modal-dialog modal-xl" role="document">
+                <div className="modal-content rounded-modal shadow p-4 border-0"
+                     style={{backgroundColor: "#bef6fd"}}
+                >
+                    <div className={"mt-4 mb-3"} style={{textAlign:"center",fontWeight: "bold"}}>
+                    <h1>Bảng Xếp Hạng</h1>
                     </div>
-                ))
-            }
 
+                    {
+                        leaderBoard.map((item, index) => (
+                            <div  key={item.id}>
+                                <div className="container">
+                                    <div className="col-auto col-centered leaderboard table-responsive">
+                                        <table className="table">
+                                            <tbody>
+                                            <tr className="shadow bg-white animated wow fadeInDown delay-0-3s">
+                                                <th scope="row" className="color-blue col-3">{index + 1}</th>
+                                                <td><img src={item.picture}
+                                                         className="rounded-circle bg-green ranker-profile "/></td>
+                                                <td className={"col-3"}>
+                                                    <h6 className="text-dark">{item.username}</h6>
+                                                </td>
+                                                <td className={"col-3"}>
+                                                    <h5 className="color-purple"><b>{item.score}</b></h5>
+                                                </td>
+                                                <td className={"col-3"}>
+                                                    <h5><Link to={`/record/+${item.recordId}`}><img src="/images/leaderboard/trophy.png"/></Link>
+                                                    </h5>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
 
+                </div>
+            </div>
         </div>
     );
 }
